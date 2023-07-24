3c9e397f51192f5c6a0fab92bf812f23f7467ed5
srbalbucio <srbalbuciogf@gmail.com>
2023-07-24 15:58:33 -0300

    Adição do comando para criar modulo implementador;

---
+++ src/main/java/balbucio/pacqit/command/impl/CreateImplementerCommand.java
@@ -0,0 +1,16 @@
+package balbucio.pacqit.command.impl;
+
+import balbucio.pacqit.Main;
+import balbucio.pacqit.command.Command;
+
+public class CreateImplementerCommand implements Command {
+    @Override
+    public String getName() {
+        return "createimplementer";
+    }
+
+    @Override
+    public void run(String[] args, Main app) {
+        app.createImplementerForm();
+    }
+}
